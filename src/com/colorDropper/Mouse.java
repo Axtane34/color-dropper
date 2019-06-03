25c01d4a43cf1c0e46db6002f1c5a9d02649c57d
Alexandr Krasin <Axtane3456@gmail.com>
2019-06-03 19:43:08 +0300

    add write file in dir bmp mechanic

--- src/com/colorDropper/Mouse.java
+++ src/com/colorDropper/Mouse.java
@@ -5,8 +5,11 @@ import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -24,6 +27,11 @@ public class Mouse extends JFrame {
         frame.setContentPane(new BgPanel());
         frame.addMouseListener(new ClickRGB());
     }
+    public static Path createFileWithDir(String directory, String filename) {
+        File dir = new File(directory);
+        if (!dir.exists()) dir.mkdirs();
+        return Paths.get(directory + File.separatorChar + filename);
+    }
 
     public class ClickRGB extends MouseAdapter implements MouseListener {
         @Override
@@ -34,18 +42,32 @@ public class Mouse extends JFrame {
                 int r = colors.getRed();
                 int g = colors.getGreen();
                 int b = colors.getBlue();
-                String hex = String.format("#%02x%02x%02x", r, g, b);
+                String hex = String.format("%02x%02x%02x", r, g, b);
                 System.out.println(hex);
                 StringSelection stringSelection = new StringSelection(hex);
                 Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
                 clipboard.setContents(stringSelection, null);
                 setVisible(false);
                 frame.dispose();
+                try {
+                    String format = "bmp";
+                    String filePath = "#" + hex + "." + format;
+                    Path path = createFileWithDir("bmp",filePath);
+                    int width = 200, height = 200;
+                    BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_BGR);
+                    Graphics2D ig2 = bi.createGraphics();
+                    ig2.setPaint(colors);
+                    ig2.fillRect(0,0,width,height);
+                    ImageIO.write(bi, format, new File(String.valueOf(path)));
+                } catch (IOException ie) {
+                    ie.printStackTrace();
+                }
             } catch (AWTException e) {
                 System.err.println(e.getMessage() + " выбор цвета");
                 System.exit(0);
             }
         }
+
     }
 
     class BgPanel extends JPanel {
